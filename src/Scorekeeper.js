8bb41c5ea111255cd1fac1ccfc5715d08b487624
mmmccormack <pmccorma@gmail.com>
2021-07-07 16:37:37 -0400

    fixed scoring and db bugs

--- src/Scorekeeper.js
+++ src/Scorekeeper.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Modal } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import firebase from './firebase';
 import smallDiamond from './assets/smallDiamond.svg';
-import diamond from './assets/fulldiamond.gif';
+import ScoringModal from './ScoringModal';
 
 
 export default function Scorekeeper() {
@@ -16,21 +15,12 @@ export default function Scorekeeper() {
 
     // modal functionality
     const [show, setShow] = useState(false);
-    const handleClose = () => {
-        setShow(false);
-        setResult("");
-        setLocation("");
-    };
+    const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    const handleHide = () => setShow(false);
+    
     // modal states
     const [frame, setFrame] = useState(0);
     const [batter, setBatter] = useState(0);
-    const [location, setLocation] = useState("");
-    const [result, setResult] = useState("");
-
-
-
     // this useEffect is added for establishing a connection to the firebase database anytime the user opens the app or makes changes to the database
     useEffect( () => {
         // establish connection to firebase DB
@@ -49,6 +39,10 @@ export default function Scorekeeper() {
             // call this function to update out component's state to be the new value
             setTeamRoster(playerRoster);
         });
+
+        return () => {
+            dbRef.off();
+        }
     }, [])
 
     const gridObj = {
@@ -87,240 +81,18 @@ export default function Scorekeeper() {
         setArray(arrayCopy);
     }
 
-    const handleCorrection = (frameResult) => {
-        const arrayCopy = [...array];
-        // take outcome from frame and split it
-        const previousResult = arrayCopy[frameResult].split(" - ");
-        const previousLocation = previousResult[1].split(",");
-        const uid = firebase.auth().currentUser.uid;
-        const dbRef = firebase.database().ref(`users/${uid}/${batter}`);
-        const update = {};
-        dbRef.on(`value`, response => {
-            if (previousLocation[1] === " R") {
-                update.runs = Number(response.val().runs) - 1;
-                setGameRuns(gameRuns - 1);
-            }
-        // update any affected fields
-        // DRAW RESULT AND FUNCTION AS ARGUMENTS INTO THE FUNCTION
-            if (previousResult[0] === "1B" || previousResult[0] === "2B" || previousResult[0] === "3B" || previousResult[0] === "HR") {
-                update.hits = Number(response.val().hits) - 1;
-                update.atbats = Number(response.val().atbats) - 1;
-                update[previousLocation[0]] = Number(response.val()[previousLocation[0]]) - 1;
-                if (previousResult[0] === "1B") {
-                    update.singles = Number(response.val().singles) - 1;
-                } else if (previousResult[0] === "2B") {
-                    update.doubles = Number(response.val().doubles) - 1;
-                } else if (previousResult[0] === "3B") {
-                    update.triples = Number(response.val().triples) - 1;
-                } else if (previousResult[0] === "HR") {
-                    update.homeruns = Number(response.val().homeruns) - 1;
-                    update.runs = Number(response.val().runs) - 1;
-                    setGameRuns(gameRuns - 1);
-                }
-            }
-            if (previousResult[0] === "BB") {
-                update.walks = Number(response.val().walks) - 1;
-            }
-            if (previousResult[0] === "SAC") {
-                update.sacs = Number(response.val().sacs) - 1;
-                if (previousLocation[0] !== "") {
-                    update[previousLocation[0]] = Number(response.val()[previousLocation[0]]) - 1;
-                }
-            }
-            if (previousResult[0] === "SO" || previousResult[0] === "OUT") {  
-                update.atbats = Number(response.val().atbats) - 1;
-                if (previousResult[0] === "SO") {
-                    update.strikeouts = Number(response.val().strikeouts) - 1;
-                } else if (previousResult[0] === "OUT" && previousLocation[0] !== "") {
-                    update.outs = Number(response.val().outs) - 1;
-                    update[previousLocation[0]] = Number(response.val()[previousLocation[0]]) - 1;
-                } else {
-                    update.outs = Number(response.val().outs) - 1;
-                }
-            }
-            if (previousResult[1] === "OUT") {
-                update.outs = Number(response.val().outs) - 1;
-            }
-                // update all percentage values
-            update.avg = (Number(response.val().hits) / Number(response.val().atbats)).toFixed(3);
-            update.obp = ((Number(response.val().hits) + Number(response.val().walks)) / (Number(response.val().atbats) + Number(response.val().walks) + Number(response.val().sacs))).toFixed(3);
-            update.slg = ((Number(response.val().singles) + (Number(response.val().doubles) * 2) + (Number(response.val().triples) * 3) + (Number(response.val().homeruns) * 4)) / Number(response.val().atbats)).toFixed(3);
-            update.ops = (Number(update.slg) + Number(update.obp)).toFixed(3);
-            
-        });
-        dbRef.off();
-        dbRef.update(update)
-        setResult("");
-        setLocation("");
-        // reset array value to zero so new modal can be displayed
-        arrayCopy[frameResult] = 0;
-        setArray(arrayCopy);
-    }
-
-    const handleScoring = (frameResult) => {
-        const arrayCopy = [...array];
-        // if the index in the array already has scoring content in it, add ot the end
-        if (typeof arrayCopy[frameResult] === "string") {
-            arrayCopy[frameResult] = `${arrayCopy[frameResult]}, ${result}`;
-        // if not, add the new outcome to the array index
-        } else {
-            arrayCopy[frameResult] = `${result} - ${location}`;
-        }
-        setArray(arrayCopy);
-        updateStats();
-        handleHide();
-    }
-
-    const updateStats = () => {
-        const uid = firebase.auth().currentUser.uid;
-        const dbRef = firebase.database().ref(`users/${uid}/${batter}`);
-        const update = {};
-        dbRef.on(`value`, response => {
-        // update any affected fields
-        // DRAW RESULT AND FUNCTION AS ARGUMENTS INTO THE FUNCTION
-            if (result === "1B" || result === "2B" || result === "3B" || result === "HR") {
-                update.hits = Number(response.val().hits) + 1;
-                update.atbats = Number(response.val().atbats) + 1;
-                update[location] = Number(response.val()[location]) + 1;
-                if (result === "1B") {
-                    update.singles = Number(response.val().singles) + 1;
-                } else if (result === "2B") {
-                    update.doubles = Number(response.val().doubles) + 1;
-                } else if (result === "3B") {
-                    update.triples = Number(response.val().triples) + 1;
-                } else if (result === "HR") {
-                    update.homeruns = Number(response.val().homeruns) + 1;
-                    update.runs = Number(response.val().runs) + 1;
-                    setGameRuns(gameRuns + 1);
-                }
-            }
-            if (result === "BB") {
-                update.walks = Number(response.val().walks) + 1;
-            }
-            if (result === "SAC") {
-                update.sacs = Number(response.val().sacs) + 1;
-                if (location !== "") {
-                    update[location] = Number(response.val()[location]) + 1;
-                }
-            }
-            if (result === "SO" || result === "OUT") {  
-                update.atbats = Number(response.val().atbats) + 1;
-                if (result === "SO") {
-                    update.strikeouts = Number(response.val().strikeouts) + 1;
-                } else if (result === "OUT" && location !== "") {
-                    update.outs = Number(response.val().outs) + 1;
-                    update[location] = Number(response.val()[location]) + 1;
-                } else {
-                    update.outs = Number(response.val().outs) + 1;
-                }
-            }
-            if (result === "R") {
-                update.runs = Number(response.val().runs) + 1;
-                setGameRuns(gameRuns + 1);
-            }
-                // update all percentage values
-            update.avg = (Number(response.val().hits) / Number(response.val().atbats)).toFixed(3);
-            update.obp = ((Number(response.val().hits) + Number(response.val().walks)) / (Number(response.val().atbats) + Number(response.val().walks) + Number(response.val().sacs))).toFixed(3);
-            update.slg = ((Number(response.val().singles) + (Number(response.val().doubles) * 2) + (Number(response.val().triples) * 3) + (Number(response.val().homeruns) * 4)) / Number(response.val().atbats)).toFixed(3);
-            update.ops = (Number(update.slg) + Number(update.obp)).toFixed(3);
-            
-        });
-        dbRef.off();
-        dbRef.update(update)
-        setResult("");
-        setLocation("");
-    }
-
-
     return (
         <>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header>
-                    <Modal.Title>{batter}, Inning {frame % 10}</Modal.Title>
-                </Modal.Header>
-                { 
-                array[frame] === 0 ? 
-                <Modal.Body>Select an area on the field where the ball was hit, and a button for what happened. Then, press "Save Result".
-                <div className="modalGrid">
-                    <div className="diamondModal">
-                        <img src={diamond} alt="A baseball diamond." />
-                        <button className="pos pitcher scoreScreen" onClick={() => setLocation("1")}>P</button>
-                        <button className="pos catcher scoreScreen" onClick={() => setLocation("2")}>C</button>
-                        <button className="pos firstbase scoreScreen" onClick={() => setLocation("3")}>1B</button>
-                        <button className="pos secondbase scoreScreen" onClick={() => setLocation("4")}>2B</button>
-                        <button className="pos thirdbase scoreScreen" onClick={() => setLocation("5")}>3B</button>
-                        <button className="pos shortstop scoreScreen" onClick={() => setLocation("6")}>SS</button>
-                        <button className="pos leftfield scoreScreen" onClick={() => setLocation("7")}>LF</button>
-                        <button className="pos centerfield scoreScreen" onClick={() => setLocation("8")}>CF</button>
-                        <button className="pos rightfield scoreScreen" onClick={() => setLocation("9")}>RF</button>
-                    </div>
-                    <div className="resultsModal">
-                    <Button variant="primary m-1" onClick={() => setResult("1B")}>
-                        Single
-                    </Button>
-                    <Button variant="primary m-1" onClick={() => setResult("2B")}>
-                        Double
-                    </Button>
-                    <Button variant="primary m-1" onClick={() => setResult("3B")}>
-                        Triple
-                    </Button>
-                    <Button variant="primary m-1" onClick={() => setResult("HR")}>
-                        HR
-                    </Button>
-                    <Button variant="primary m-1" onClick={() => setResult("BB")}>
-                        BB
-                    </Button>
-                    <Button variant="primary m-1" onClick={() => setResult("SAC")}>
-                        SAC
-                    </Button>
-                    <Button variant="primary m-1" onClick={() => setResult("SO")}>
-                        SO
-                    </Button>
-                    <Button variant="primary m-1" onClick={() => setResult("OUT")}>
-                        OUT
-                    </Button>
-                    </div>
-                    <div className="emptyModal">
-                        Result: {result} {location === "" ? null : `to ${location}`}
-                    </div>
-                </div>
-                </Modal.Body> 
-                : 
-                <Modal.Body>Update your player's outcome for this frame, or select "Correction" to re-score this frame.
-                <div className="modalGrid">
-                    <div className="diamondModal">
-                    </div>
-                    <div className="resultsModal">
-                        <Button variant="primary m-1" onClick={() => setResult("R")}>
-                            R
-                        </Button>
-                        <Button variant="primary m-1" onClick={() => setResult("OUT")}>
-                            OUT
-                        </Button>
-                    </div>
-                    <div className="emptyModal">
-                        Result: {result}
-                    </div>
-                </div>
-                </Modal.Body> }
-                
-                <Modal.Footer>
-                    {
-                    array[frame] !== 0 ? 
-                    <Button variant="warning" onClick={() => handleCorrection(frame)}>
-                    Correction
-                    </Button> 
-                    : null
-                    }
-                
-                <Button variant="danger" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="success" onClick={() => handleScoring(frame, result)}>
-                    Save Result
-                </Button>
-                </Modal.Footer>
-            </Modal>
+            <ScoringModal
+                frame={frame}
+                batter={batter}
+                array={array}
+                show={show}
+                hideModal={handleClose}
+                setArray={setArray}
+                gameRuns={gameRuns}
+                setGameRuns={setGameRuns}
+            />
             <div className="score">
                 <div className="yourTeam">
                     <p>Your team:</p>
@@ -377,7 +149,7 @@ export default function Scorekeeper() {
                                     backgroundImage: `url(${smallDiamond})`, 
                                     backgroundRepeat: `no-repeat`,
                                     backgroundPosition: `center`}}
-                                className="btn btn-success w-100" 
+                                className="btn btn-success w-100 p-0" 
                                 onClick={() => showCell(index)}
                                 >
                                 {cellValue}
